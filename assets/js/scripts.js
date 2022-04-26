a39e65a80adb5c18fbcb19ed10f7e7d5b062a347
impshum <impshum@impshum.co.uk>
2022-04-26 17:51:07 +0100

    q

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,7 +9,7 @@ const modal_url = document.getElementById('modal_url');
 
 async function get_json(file) {
   try {
-    const req = await fetch('assets/json/' + file + '.json');
+    const req = await fetch(`assets/json/${file}.json`);
     return await req.json();
 
   } catch (err) {
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
     sectionSelector: '.slide_section',
     navigation: true,
     navigationPosition: 'right',
-    navigationTooltips: ['0', '1', '2', '3'],
+    //navigationTooltips: ['0', '1', '2', '3'],
     onLeave: function(origin, destination, direction) {
       console.log(origin);
       if (destination.item.getElementsByClassName('title')[0]) {
@@ -44,70 +44,91 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
   fullpage_api.setAllowScrolling(true);
 
-  // Functions to open and close a modal
-  function openModal($el) {
-    $el.classList.add('is-active');
-  }
-
-  function closeModal($el) {
-    $el.classList.remove('is-active');
-  }
-
-  function closeAllModals() {
-    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-      closeModal($modal);
-    });
-  }
-
-  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-    const modal = $trigger.dataset.target;
-    const $target = document.getElementById(modal);
-    console.log($target);
-
-    $trigger.addEventListener('click', () => {
-      openModal($target);
-    });
-  });
-
-  (document.querySelectorAll('.modal-background, .modal-close, .modal-close-button, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-    const $target = $close.closest('.modal');
-
-    $close.addEventListener('click', () => {
-      closeModal($target);
-    });
-  });
-
-  document.addEventListener('keydown', (event) => {
-    const e = event || window.event;
-
-    if (e.keyCode === 27) { // Escape key
-      closeAllModals();
-    }
-  });
-
-  let project_links = document.getElementsByClassName('project_link');
-
-  for (var i = 0; i < project_links.length; i++) {
-    project_links[i].addEventListener('click', (e) => {
-      modal.classList.add('is-active');
-      modal_title.textContent = e.target.getAttribute('data-title');
-      modal_start_date.textContent = e.target.getAttribute('data-start_date');
-      modal_end_date.textContent = e.target.getAttribute('data-end_date');
-      modal_info.textContent = e.target.getAttribute('data-info');
-      var url = e.target.getAttribute('data-url');
-      if (url != 'false') {
-        modal_url.href = e.target.getAttribute('data-url');
-        modal_url.parentNode.classList.remove('is-hidden');
-      } else {
-        modal_url.parentNode.classList.add('is-hidden');
-      }
-    });
-  }
-
   dontGo({
     title: 'Shaun Kollannur - Machine Learning Researcher',
     faviconSrc: 'assets/img/favicon2.png',
     timeout: 1000
   });
+
+  get_json('projects')
+    .then((res) => {
+      console.log(res);
+      let projects_html = '';
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i]);
+        projects_html += `<li class="animate__animated animate__faster">
+          <a class="project_link" data-title="${res[i].title}" data-start_date="${res[i].start_date}" data-end_date="${res[i].end_date}"
+            data-info="${res[i].info}"
+            data-url="${res[i].url}">
+            <i class="project_link fa-solid fa-arrow-up-right-from-square" data-title="${res[i].title}" data-start_date="${res[i].start_date}" data-end_date="${res[i].end_date}"
+              data-info="${res[i].info}"
+              data-url="${res[i].url}"></i>
+            ${res[i].title}
+          </a>
+        </li>`
+      }
+      project_container.innerHTML = projects_html;
+
+      let project_links = document.getElementsByClassName('project_link');
+
+      for (var i = 0; i < project_links.length; i++) {
+        project_links[i].addEventListener('click', (e) => {
+          modal.classList.add('is-active');
+          modal_title.textContent = e.target.getAttribute('data-title');
+          modal_start_date.textContent = e.target.getAttribute('data-start_date');
+          modal_end_date.textContent = e.target.getAttribute('data-end_date');
+          modal_info.textContent = e.target.getAttribute('data-info');
+          var url = e.target.getAttribute('data-url');
+          if (url != 'false') {
+            modal_url.href = e.target.getAttribute('data-url');
+            modal_url.parentNode.classList.remove('is-hidden');
+          } else {
+            modal_url.parentNode.classList.add('is-hidden');
+          }
+        });
+      }
+    });
+
+//d
+    // Functions to open and close a modal
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
+
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+      });
+    }
+
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+      const modal = $trigger.dataset.target;
+      const $target = document.getElementById(modal);
+      console.log($target);
+
+      $trigger.addEventListener('click', () => {
+        openModal($target);
+      });
+    });
+
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-close-button, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+      const $target = $close.closest('.modal');
+
+      $close.addEventListener('click', () => {
+        closeModal($target);
+      });
+    });
+
+    document.addEventListener('keydown', (event) => {
+      const e = event || window.event;
+
+      if (e.keyCode === 27) { // Escape key
+        closeAllModals();
+      }
+    });
 
 })
